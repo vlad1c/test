b2b4a18e8ed521098b5d18530cbad772f88b5688
vlad1c <test@vlad1c.ru>
2017-04-14 17:02:52 +0300

    добавил коммент

--- подбор цвета.ps1
+++ подбор цвета.ps1
@@ -3,5 +3,6 @@ $Host.UI.RawUI.BackgroundColor = "DarkGray"
 $Host.UI.RawUI.CursorSize = 5
 $Host.UI.RawUI.WindowTitle = "My Window"
 #пиу пиу11111
+#трям
 Get-Content "D:\Dropbox\Util\Скрипты\Загрузить настройки.ps1" 
 Read-Host
